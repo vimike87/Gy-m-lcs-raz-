d0e873a7fa8454e703a7e8ffb2fc88158505f2f0
vimike87 <vimike87@gmail.com>
2023-03-10 18:07:21 +0100

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace GyumolcsArazo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            btAlma = new Button();
+            flowLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(btAlma);
+            flowLayoutPanel1.Dock = DockStyle.Top;
+            flowLayoutPanel1.Location = new Point(0, 0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(720, 242);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btAlma
+            // 
+            btAlma.Location = new Point(3, 3);
+            btAlma.Name = "btAlma";
+            btAlma.Size = new Size(132, 121);
+            btAlma.TabIndex = 0;
+            btAlma.Text = "400";
+            btAlma.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(720, 372);
+            Controls.Add(flowLayoutPanel1);
+            Name = "Form1";
+            Text = "Gyümölcsárazó";
+            flowLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button btAlma;
+    }
+}
