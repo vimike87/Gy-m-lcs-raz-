3ffd3cca9dce22af7720ee8386b1ecd7b324fd04
vimike87 <vimike87@gmail.com>
2023-03-28 12:58:47 +0200

    Project finished

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,14 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             flowLayoutPanel1 = new FlowLayoutPanel();
             btAlma = new Button();
+            BtKorte = new Button();
+            BtBanan = new Button();
+            btNarancs = new Button();
+            tbTomeg = new TextBox();
+            llAr = new Label();
+            label1 = new Label();
             flowLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
             flowLayoutPanel1.Controls.Add(btAlma);
+            flowLayoutPanel1.Controls.Add(BtKorte);
+            flowLayoutPanel1.Controls.Add(BtBanan);
+            flowLayoutPanel1.Controls.Add(btNarancs);
             flowLayoutPanel1.Dock = DockStyle.Top;
             flowLayoutPanel1.Location = new Point(0, 0);
             flowLayoutPanel1.Name = "flowLayoutPanel1";
@@ -44,28 +54,99 @@
             // 
             // btAlma
             // 
+            btAlma.Image = (Image)resources.GetObject("btAlma.Image");
             btAlma.Location = new Point(3, 3);
             btAlma.Name = "btAlma";
             btAlma.Size = new Size(132, 121);
             btAlma.TabIndex = 0;
             btAlma.Text = "400";
             btAlma.UseVisualStyleBackColor = true;
+            btAlma.Click += bt_Click;
+            // 
+            // BtKorte
+            // 
+            BtKorte.Image = (Image)resources.GetObject("BtKorte.Image");
+            BtKorte.Location = new Point(141, 3);
+            BtKorte.Name = "BtKorte";
+            BtKorte.Size = new Size(132, 121);
+            BtKorte.TabIndex = 1;
+            BtKorte.Text = "700";
+            BtKorte.UseVisualStyleBackColor = true;
+            BtKorte.Click += bt_Click;
+            // 
+            // BtBanan
+            // 
+            BtBanan.Image = (Image)resources.GetObject("BtBanan.Image");
+            BtBanan.Location = new Point(279, 3);
+            BtBanan.Name = "BtBanan";
+            BtBanan.Size = new Size(132, 121);
+            BtBanan.TabIndex = 2;
+            BtBanan.Text = "300";
+            BtBanan.UseVisualStyleBackColor = true;
+            BtBanan.Click += bt_Click;
+            // 
+            // btNarancs
+            // 
+            btNarancs.Image = (Image)resources.GetObject("btNarancs.Image");
+            btNarancs.Location = new Point(417, 3);
+            btNarancs.Name = "btNarancs";
+            btNarancs.Size = new Size(132, 121);
+            btNarancs.TabIndex = 3;
+            btNarancs.Text = "500";
+            btNarancs.UseVisualStyleBackColor = true;
+            btNarancs.Click += bt_Click;
+            // 
+            // tbTomeg
+            // 
+            tbTomeg.Location = new Point(29, 258);
+            tbTomeg.Name = "tbTomeg";
+            tbTomeg.PlaceholderText = "Kg";
+            tbTomeg.Size = new Size(150, 31);
+            tbTomeg.TabIndex = 1;
+            // 
+            // llAr
+            // 
+            llAr.AutoSize = true;
+            llAr.Location = new Point(251, 264);
+            llAr.Name = "llAr";
+            llAr.Size = new Size(22, 25);
+            llAr.TabIndex = 2;
+            llAr.Text = "0";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(194, 264);
+            label1.Name = "label1";
+            label1.Size = new Size(34, 25);
+            label1.TabIndex = 3;
+            label1.Text = "Ár:";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(720, 372);
+            Controls.Add(label1);
+            Controls.Add(llAr);
+            Controls.Add(tbTomeg);
             Controls.Add(flowLayoutPanel1);
             Name = "Form1";
             Text = "Gyümölcsárazó";
             flowLayoutPanel1.ResumeLayout(false);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private FlowLayoutPanel flowLayoutPanel1;
         private Button btAlma;
+        private Button BtKorte;
+        private Button BtBanan;
+        private Button btNarancs;
+        private TextBox tbTomeg;
+        private Label llAr;
+        private Label label1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,35 @@ namespace GyumolcsArazo
         public Form1()
         {
             InitializeComponent();
+            var btLista = flowLayoutPanel1.Controls.OfType<Button>().ToList();
+            foreach (var bt in btLista)
+                KepetIgazit(bt);
+        }
+
+        private void KepetIgazit(Button bt)
+        {
+            Image AtmeretezettKep = new Bitmap(bt.Image, new Size(bt.Width - 5, bt.Height - 5));
+            bt.Image = AtmeretezettKep;
+        }
+
+        private void BtKorte_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bt_Click(object sender, EventArgs e)
+        {
+            Button bt = (Button)sender;
+            //Alternative solution: Button bt= sender as  Button
+            if (double.TryParse(tbTomeg.Text, out double tomeg))
+            {
+
+
+                double ar = tomeg * double.Parse(bt.Text);
+                llAr.Text = ar.ToString();
+
+
+            };
         }
     }
 }
